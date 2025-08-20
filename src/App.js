b04adcdd7371ca709132c4b40b1d8948f08b8458
root <root@DESKTOP-CUQIG2B.localdomain>
2025-08-19 23:13:07 -0400

    putting up construction image

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './wip.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} />
+      </header>
+    </div>
+  );
+}
+
+export default App;
